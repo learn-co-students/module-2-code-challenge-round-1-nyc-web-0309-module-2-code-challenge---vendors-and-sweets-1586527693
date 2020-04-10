a9d7398f3e932df9869b03a03cfc747f782179af
Yahyagok <ygok17@hotmail.com>
2020-04-10 11:59:01 -0400

    last

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,13 @@
 class Vendor < ApplicationRecord
     has_many :vendor_sweets
     has_many :sweets, through: :vendor_sweets
+
+
+    def avarage_price_sweet
+        total = 0 
+     self.vendor_sweets.each do |vendor_sweet|
+         total += vendor_sweet.price 
+    end 
+      total 
+    end 
 end
