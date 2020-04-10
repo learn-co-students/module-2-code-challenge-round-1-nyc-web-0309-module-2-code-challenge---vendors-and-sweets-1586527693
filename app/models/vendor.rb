50a2a099fed17927d6a523e6beceedac6f8c0c4e
codenameuriel <u.e.rodriguez@gmail.com>
2020-04-10 11:46:58 -0400

    progress 2

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,29 @@
 class Vendor < ApplicationRecord
   has_many :vendor_sweets
   has_many :sweets, through: :vendor_sweets 
+
+
+# the average price for all Sweets at the top of the page
+# the average price for each Vendor next to the Vendor's name
+
+  def self.average_price_of_sweets
+    price = []
+    self.all.each do |vendor|
+      vendor.vendor_sweets.each do |v_s|
+        price << v_s.price
+      end
+    end
+
+    price.reduce(0.0) do |sum, price|
+      sum + price
+    end / price.length
+  end
+
+  def average_price
+    self.vendor_sweets.reduce(0.0) do |sum, vendor_sweet|
+      sum + vendor_sweet.price 
+    end / self.vendor_sweets.length
+  end
+
+
 end
--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -4,13 +4,5 @@ class VendorSweet < ApplicationRecord
 
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
-  validate :duplicate
-
-  def duplicate
-    VendorSweet.all.each do |vendor_sweet|
-      if vendor_sweet.include?(vendor_id) && vendor_sweet.include?(sweet.id)
-        errors << "Vendor with that Sweet pairing already exists"
-      end
-    end
-  end
+  validates :vendor_id, uniqueness: { scope: :sweet_id }
 end
