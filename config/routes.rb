c0294d86d934eb2cee5e641c5d9d2ffe7a0cbc94
CBreakr <github email address>
2020-04-10 11:53:54 -0400

    removed extraneous route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get '/vendors', to: "vendors#index", as: "vendors"
   get "/vendors/:id", to: "vendors#show", as: "vendor"
 
-  # get "/vendorsweets", to: "vendorSweets#index", as: "vendorsweets"
   get "/vendor_sweets/new", to: "vendor_sweets#new", as: "new_vendor_sweet"
   post "/vendor_sweets", to: "vendor_sweets#create", as: "vendor_sweets"
 end
