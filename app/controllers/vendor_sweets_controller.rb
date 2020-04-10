003f325ce234dd1ea9bca790615fa2e4a32e8589
Eliot Henson <eliotrhenson@gmail.com>
2020-04-10 11:03:05 -0400

    Having trouble with seed data though I think my relationship is correct

---
+++ app/controllers/vendor_sweets_controller.rb
@@ -0,0 +1,3 @@
+class VendorSweetsController < ApplicationController
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,10 @@
 Vendor.destroy_all
 Sweet.destroy_all
 
+
 vendors = ["Insomnia Cookies", "Cookies Cream","Carvel","Gregory's Coffee","Duane Park Patisserie","Tribeca Treats"]
 sweets = ["Chocolate Chip Cookie", "Chocolate Chunk Cookie","M&Ms Cookie","White Chocolate Cookie","Brownie","Peanut Butter Icecream Cake", ]
+
 
 vendors.each do |vendor|
   Vendor.create(name: vendor)
@@ -19,3 +21,11 @@ end
 sweets.each do |sweet|
   Sweet.create(name: sweet)
 end
+
+
+
+
+
+vendor1 = Vendor.create(name: "Chocolate")
+sweet1 = Sweet.create(name: "Chococloate_chip")
+vendorsweet1 = VendorSweet.create(sweet_id: Sweet.all.sample.id, vendor_id: Vendor.all.sample.id, price: 5)
