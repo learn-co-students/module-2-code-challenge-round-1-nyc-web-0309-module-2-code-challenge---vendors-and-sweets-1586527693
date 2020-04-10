4e6efb0f5afe2544c20e3f36941f7b4602e982eb
diana2341 <dianaponce2341@gmail.com>
2020-04-10 11:59:09 -0400

    done

--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -2,6 +2,7 @@ class VendorSweet < ApplicationRecord
     belongs_to :vendor 
     belongs_to :sweet
     validates :price, presence:true 
+    
     validates :price, numericality: {greater_than:0}
     # validates :vendor, uniqueness:true
     # validates :sweet, uniqueness:true
