7ebcf8cf00311974f64bcbf057a762745a0833e9
Yueying <wangyy395@gmail.com>
2020-04-10 11:01:30 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :vendor_sweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/sweets', to: "sweets#index", as: "sweets"
+  get '/sweets/:id', to: "sweets#show", as: "sweet"
   get '/vendors', to: "vendors#index", as: "vendors"
+  get '/vendors/:id', to: "vendors#show", as: "vendor"
+
 
 end
